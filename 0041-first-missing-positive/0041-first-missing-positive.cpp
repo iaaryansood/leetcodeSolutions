f0cd02928f0607945b9ade9d6cb4d6073d0e22dc
iaaryansood <bobmarley86325@gmail.com>
2024-08-14 18:05:13 +0530

    Time: 44 ms (94.23%), Space: 54 MB (34.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int> map;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i] > 0)
-            {
-                map[nums[i]]++;
-            }   
-        }
-        int ans=1;
-        for(auto it:map)
-        {
-            // cout<<it.first<<" ";
-            if(it.first == ans)
-            {
-                ans++;
-            }
-            else
-            {
-                return ans;
+        int n = nums.size();
+        vector<bool> seen(n + 1, false);  // Array for lookup
+
+        // Mark the elements from nums in the lookup array
+        for (int num : nums) {
+            if (num > 0 && num <= n) {
+                seen[num] = true;
             }
         }
-        return ans;
+
+        // Iterate through integers 1 to n
+        // return smallest missing positive integer
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
+        }
+
+        // If seen contains all elements 1 to n
+        // the smallest missing positive number is n + 1
+        return n + 1;
     }
 };
