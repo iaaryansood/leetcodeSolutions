d0cdf3659d1388ff091508f83f51e9623d6c5d7e
iaaryansood <bobmarley86325@gmail.com>
2024-08-14 17:48:37 +0530

    Time: 276 ms (5.07%), Space: 79.8 MB (5.22%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int firstMissingPositive(vector<int>& nums) {
+        map<int,int> map;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] > 0)
+            {
+                map[nums[i]]++;
+            }   
+        }
+        int ans=1;
+        for(auto it:map)
+        {
+            cout<<it.first<<" ";
+            if(it.first == ans)
+            {
+                ans++;
+            }
+            else
+            {
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
