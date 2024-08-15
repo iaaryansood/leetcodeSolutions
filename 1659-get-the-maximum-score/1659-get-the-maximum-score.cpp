303da01a72213e04a4bea6900c9bcb81496fcc9e
iaaryansood <bobmarley86325@gmail.com>
2024-08-15 23:25:37 +0530

    Time: 71 ms (76.1%), Space: 58.4 MB (66.73%) - LeetHub

---
+++ 1659-get-the-maximum-score/1659-get-the-maximum-score.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size(),i=0;
+        int m=nums2.size(),j=0;
+        long  sum1 = 0,sum2=0;
+        long ans=0 , mod =1e9 + 7;
+        while(i < n && j < m)
+        {
+            if(nums1[i] == nums2[j])
+            {
+                ans+=max(sum1,sum2) + nums1[i];
+                sum1=0,sum2=0;
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j])
+            {
+                sum1+=nums1[i++];
+            }
+            else
+            {
+                sum2+=nums2[j++];
+            }
+        }
+        while(j < m)
+        {
+            sum2+=nums2[j++];
+        }
+        while(i < n)
+        {
+            sum1+=nums1[i++];
+        }
+        return (ans + max(sum1,sum2)) % mod;
+    }
+};
