213987d3311a93bf9732d5159067cde813b5f396
iaaryansood <bobmarley86325@gmail.com>
2024-08-21 02:21:30 +0530

    Time: 8 ms (54.92%), Space: 10.7 MB (25.43%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,48 +1,41 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int s1Len = s1.size();
-        int s2Len = s2.size();
+        if(s1.size() > s2.size())
+        return false;
 
-        if (s1Len > s2Len) return false;  // If s1 is longer than s2, impossible to have a permutation.
-
-        unordered_map<char, int> s1Map, s2Map;
-
-        // Count frequencies of each character in s1
-        for (char c : s1) {
-            s1Map[c]++;
+        unordered_map<char,int> s1map;
+        for(int i=0;i<s1.size();i++)
+        {
+            s1map[s1[i]]++;
         }
 
-        // Initialize the frequency map for the first window in s2
-        for (int i = 0; i < s1Len; i++) {
-            s2Map[s2[i]]++;
+        unordered_map<char,int> s2map;
+
+        for(int i=0;i < s1.size();i++)
+        {
+            s2map[s2[i]]++;
         }
 
-        // Check if the first window matches s1's character frequency
-        if (s1Map == s2Map) return true;
+        if(s1map == s2map)
+        return true;
 
-        // Slide the window over s2
-        for (int i = s1Len; i < s2Len; i++) {
-            // Add the new character in the window
-            char newChar = s2[i];
-            s2Map[newChar]++;
-            
-            // Remove the old character from the window
-            char oldChar = s2[i - s1Len];
-            if (s2Map[oldChar] == 1) {
-                s2Map.erase(oldChar);  // Remove entry if count is zero
-            } else {
-                s2Map[oldChar]--;
+        for(int i=s1.size();i<s2.size();i++)
+        {
+            char newChar=s2[i];
+            s2map[newChar]++;
+            char oldChar=s2[i-s1.size()];
+            if(s2map[oldChar] == 1)
+            {
+                s2map.erase(oldChar);
             }
-
-            // Check if the current window matches s1's character frequency
-            if (s1Map == s2Map) return true;
+            else
+            {
+                s2map[oldChar]--;
+            }
+            if(s1map == s2map)
+            return true;
         }
-
-        return false;  // No permutation found
+        return false;
     }
 };
