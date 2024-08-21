951559565fdf8b6904c9321975707e68b960fec0
iaaryansood <bobmarley86325@gmail.com>
2024-08-21 15:00:24 +0530

    Time: 7 ms (69.86%), Space: 9.6 MB (26.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) { 
+    int characterReplacement(string s, int k) {
+        // slidding window technique
+        int left=0,right=0;
+        int n=s.size();
+        unordered_map<char,int> map;
         int maxlen=0;
-        for(int i=0;i<s.size();i++)
+        int maxf=0;
+        while(right < n)
         {
-            vector<int> map(26,0);
-            int maxf=0;
-            for(int j=i;j<s.size();j++)
+            int len=right-left+1;
+            map[s[right]]++;
+            maxf=max(maxf,map[s[right]]);
+            int changes=len-maxf;
+            if(changes <= k)
             {
-                int len=j-i+1;
-                map[s[j] - 'A']++;
-                maxf=max(maxf,map[s[j] - 'A']);
-                int changes=len - maxf;
-                if(changes <= k)
-                {
-                    maxlen=max(maxlen,len);
-                }
-                else
-                {
-                    break;
-                }
+                maxlen=max(maxlen,len);
             }
+            else
+            {
+                map[s[left]]--;
+                left++;
+                int ans=INT_MIN;
+                unordered_map<char,int> :: iterator it;
+                for(it=map.begin();it != map.end();it++)
+                {
+                    if(it->second > ans)
+                    {
+                        ans=it->second;
+                    }
+                }
+                maxf=ans;
+            }
+            right++;
         }
         return maxlen;
     }
