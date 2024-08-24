fec8abaf961c4d28512814480979841064bc2403
iaaryansood <bobmarley86325@gmail.com>
2024-08-25 00:33:07 +0530

    Time: 43 ms (9.81%), Space: 37.9 MB (5.43%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
 
-    int isenough(vector<int> piles,double k)
+    int isenough(vector<int> piles,int k)
     {
         int ans=0;
         for(int i=0;i<piles.size();i++)
         {
             ans+=ceil(piles[i]/k);
+            if(piles[i] % k != 0)
+            {
+                ans++;
+            }
         }
         return ans;
     }
@@ -16,7 +20,7 @@ public:
         int right=*max_element(piles.begin(),piles.end());
         while(left < right)
         {
-            double mid=left+(right-left)/2;
+            int mid=left+(right-left)/2;
             // cout<<left<<" "<<mid<<" "<<right<<endl;
             int required=isenough(piles,mid);
             // cout<<"required : "<<required<<endl;
