5c556ea5d29ca2492ec4ad89404c03a3268da1db
iaaryansood <bobmarley86325@gmail.com>
2024-08-30 00:29:54 +0530

    Time: 118 ms (94.25%), Space: 61 MB (93.64%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,31 +1,50 @@
+#include <vector>
+#include <cstdlib> // For rand()
+#include <algorithm> // For swap()
+
+using namespace std;
+
 class Solution {
 public:
+    // Helper function to compute the squared distance from the origin
+    int squaredDistance(const vector<int>& point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+
+    // Quickselect function
+    void quickSelect(vector<vector<int>>& points, int left, int right, int k) {
+        if (left >= right) return;
+
+        // Choose a random pivot
+        int pivotIndex = left + rand() % (right - left + 1);
+        int pivotDist = squaredDistance(points[pivotIndex]);
+
+        // Move pivot to the end
+        swap(points[pivotIndex], points[right]);
+        int storeIndex = left;
+
+        // Partition the list
+        for (int i = left; i < right; ++i) {
+            if (squaredDistance(points[i]) <= pivotDist) {
+                swap(points[storeIndex], points[i]);
+                ++storeIndex;
+            }
+        }
+        // Move pivot to its final place
+        swap(points[right], points[storeIndex]);
+
+        // Recursively find the k closest points
+        int leftCount = storeIndex - left + 1;
+        if (k < leftCount) {
+            quickSelect(points, left, storeIndex - 1, k);
+        } else if (k > leftCount) {
+            quickSelect(points, storeIndex + 1, right, k - leftCount);
+        }
+    }
+
+    // Main function to find k closest points
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        unordered_map<int,vector<vector<int>>> map;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int i=0;i<points.size();i++)
-        {
-            int sum=0;
-            for(int j=0;j<points[i].size();j++)
-            {
-                sum+=pow(points[i][j],2);
-            }
-            map[sum].push_back(points[i]);
-            pq.push(sum);
-        }
-        vector<vector<int>> ans;
-        while(k)
-        {
-            int dist=pq.top();
-            pq.pop();
-            for(auto points : map[dist])
-            {
-                ans.push_back(points);
-                k--;
-                if(k == 0) break;
-            }
-            
-        }
-        return ans;
+        quickSelect(points, 0, points.size() - 1, k);
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
