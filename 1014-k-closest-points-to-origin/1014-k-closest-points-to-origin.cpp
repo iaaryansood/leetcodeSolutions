52860f3b6da0af55ad9436cd4c8578ef249e5d82
iaaryansood <bobmarley86325@gmail.com>
2024-08-30 02:51:41 +0530

    Time: 167 ms (46.77%), Space: 78 MB (35.35%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
-        priority_queue<int,vector<int>,greater<int>> pq;
-        unordered_map<int,vector<vector<int>>> map;
-        for(int i=0;i<points.size();i++)
+    int squareDistance(vector<int> point)
+    {
+        return point[0] * point[0] + point[1] * point[1]; 
+    }
+
+    void quickselect(vector<vector<int>> &points,int left,int right,int k)
+    {
+        if(left >= right)   return;
+
+        int pivotIndex=left + rand() % (right-left+1);
+        int pivotDistance=squareDistance(points[pivotIndex]);
+
+        swap(points[right],points[pivotIndex]);
+        int storeIndex=left;
+
+        for(int i=left;i<right;i++)
         {
-            int sum=0;
-            for(int j=0;j<points[i].size();j++)
+            if(squareDistance(points[i]) <= pivotDistance)
             {
-                sum+=pow(points[i][j],2);
+                swap(points[i],points[storeIndex]);
+                storeIndex++;
             }
-            pq.push(sum);
-            map[sum].push_back(points[i]);
         }
-        vector<vector<int>> ans;
-        while(k)
+        swap(points[right],points[storeIndex]);
+
+        int leftCount=storeIndex-left+1;
+        if(leftCount == k)
         {
-            int dist=pq.top();
-            pq.pop();
-            for(auto point : map[dist])
-            {
-                ans.push_back(point);
-                k--;
-                if(k == 0) break;
-            }
+            return;
         }
-        return ans;
+        else if(leftCount < k)
+        {
+            quickselect(points,storeIndex+1,right,k-leftCount);
+        }
+        else
+        {
+            quickselect(points,left,storeIndex-1,k);
+        }
+
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        quickselect(points,0,points.size()-1,k);
+        return vector<vector<int>>(points.begin(),points.begin()+k);
     }
 };
