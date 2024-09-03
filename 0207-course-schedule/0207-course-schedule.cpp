f91eb89b00f6766d2ab29c51d1f95b281c786c17
iaaryansood <bobmarley86325@gmail.com>
2024-09-03 22:56:46 +0530

    Time: 19 ms (35.61%), Space: 17.5 MB (63.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,44 +1,54 @@
 class Solution {
-private:
-    bool dfs(int node, vector<vector<int>> &adjlist, vector<int> &visited) {
-        visited[node] = 1;  // Mark the node as being visited (part of the current path)
+private :
+    bool dfs(int node,vector<vector<int>> &adjlist,vector<int> &visited)
+    {
+        visited[node]=1;
 
-        for (int temp : adjlist[node]) {
-            if (visited[temp] == 0) {
-                // If the node has not been visited yet, recursively visit it
-                if (dfs(temp, adjlist, visited)) {
-                    return true;  // A cycle was detected
+        for(int i=0;i<adjlist[node].size();i++)
+        {
+            int temp=adjlist[node][i];
+            if(visited[temp] == 0)
+            {
+                // visited[temp]=1;
+                if(dfs(temp,adjlist,visited))
+                {
+                    return true;
                 }
-            } else if (visited[temp] == 1) {
-                // If the node is currently being visited, a cycle is detected
+            }
+            else if(visited[temp] == 1)
+            {
                 return true;
             }
         }
-
-        visited[node] = 2;  // Mark the node as fully processed
+        visited[node]=2;
         return false;
     }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjlist(numCourses);
-        vector<int> visited(numCourses, 0);
+        vector<int> visited(numCourses,0);
 
-        // Construct the adjacency list
-        for (int i = 0; i < prerequisites.size(); i++) {
-            int course = prerequisites[i][0];
-            int prerequisite = prerequisites[i][1];
-            adjlist[course].push_back(prerequisite);
+        vector<vector<int>> adjlist(numCourses);
+
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            for(int j=0;j<prerequisites[i].size();j++)
+            {
+                int course=prerequisites[i][1];
+                int prerequisite=prerequisites[i][0];
+                adjlist[course].push_back(prerequisite);
+            }
         }
 
-        // Perform DFS for each unvisited node
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if (dfs(i, adjlist, visited)) {
-                    return false;  // A cycle was detected
+        for(int i=0;i<visited.size();i++)
+        {
+            if(visited[i] == 0)
+            {
+                if(dfs(i,adjlist,visited))
+                {
+                    return false;
                 }
             }
         }
-        return true;  // No cycle was detected
+        return true;
     }
 };
