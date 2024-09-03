9ff15c9038d7456714edc10b1ffce17512d5b98f
iaaryansood <bobmarley86325@gmail.com>
2024-09-04 04:29:22 +0530

    Time: 57 ms (42%), Space: 38.2 MB (44.89%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    void bfs(int n,int m,vector<vector<int>> &distance,queue<pair<pair<int,int>,int>> &q,vector<vector<int>> &visited)
+    {
+        vector<pair<int,int>> directions={{-1,0},{0,-1},{1,0},{0,1}};
+        while(!q.empty())
+        {
+            pair<pair<int,int>,int> p=q.front();
+            q.pop();
+
+            int row=p.first.first;
+            int col=p.first.second;
+            int depth=p.second;
+
+            distance[row][col]=depth;
+
+            for(int i=0;i<4;i++)
+            {
+                int delrow=row+directions[i].first;
+                int delcol=col+directions[i].second;
+
+                if(delrow >=0 && delcol >= 0 && delrow < n && delcol < m && !visited[delrow][delcol])
+                {
+                    q.push(pair<pair<int,int>,int>(pair<int,int>(delrow,delcol),depth+1));
+                    visited[delrow][delcol]=1;
+                }
+            }
+
+        }
+    }
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        vector<vector<int>> distance(n,vector<int>(m,-1));
+
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i=0;i<mat.size();i++)
+        {
+            for(int j=0;j<mat[i].size();j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    visited[i][j]=1;
+                    // q.push(pair<pair<int,int>>(i,j),0);
+                    q.push(pair<pair<int,int>,int>(pair<int,int>(i,j),0));
+                }
+            }
+        }
+
+        bfs(n,m,distance,q,visited);
+
+        return distance;
+    }
+};
