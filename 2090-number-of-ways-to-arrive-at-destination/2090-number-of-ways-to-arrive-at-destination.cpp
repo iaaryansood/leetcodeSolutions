9e49eef6c9827dbba7711cbc1c9d643afed690fd
iaaryansood <bobmarley86325@gmail.com>
2024-09-15 23:31:47 +0530

    Time: 33 ms (99.91%), Space: 37.2 MB (37.03%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int,long long>>> adj(n);
+
         for(int i=0;i<roads.size();i++)
         {
-            adj[roads[i][0]].push_back(pair<int,int>(roads[i][1],roads[i][2]));
-            adj[roads[i][1]].push_back(pair<int,int>(roads[i][0],roads[i][2]));
+            adj[roads[i][0]].push_back(pair<int,long long>(roads[i][1],roads[i][2]));
+            adj[roads[i][1]].push_back(pair<int,long long>(roads[i][0],roads[i][2]));
         }
 
-        vector<long long> distance(n,LLONG_MAX);
-        distance[0]=0;
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+        pq.push({0,0});
 
         vector<int> count(n,0);
         count[0]=1;
 
-        int MOD=1e9 + 7;
+        vector<long long> distance(n,LLONG_MAX);
+        distance[0]=0;
 
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
-        pq.push(pair<int,int>(0,0));
+        int MOD=1e9 + 7;
 
         while(!pq.empty())
         {
@@ -35,15 +36,15 @@ public:
                 if(rootDistance + edgeWeight < distance[temp])
                 {
                     distance[temp]=rootDistance + edgeWeight;
-                    count[temp]=count[node];
                     pq.push(pair<long long,int>(distance[temp],temp));
+                    count[temp]=count[node];
                 }
-                else if((long long)rootDistance + edgeWeight == distance[temp])
+                else if(rootDistance + edgeWeight == distance[temp])
                 {
-                    count[temp]= (count[temp] + count[node]) % MOD;
+                    count[temp]=(count[temp] + count[node]) % MOD;
                 }
             }
         }
-        return count[n-1] ;
+        return count[n-1];
     }
 };
