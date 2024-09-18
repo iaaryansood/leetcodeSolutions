a588df134a82a096b2d5501b52820a1a5e1c2eb9
iaaryansood <bobmarley86325@gmail.com>
2024-09-18 23:23:16 +0530

    Time: 26 ms (21.59%), Space: 18.5 MB (69.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,102 @@
-class Solution {
-public:
-
-    void dfs(int node,vector<int> &visited,vector<vector<int>> &isConnected)
+class DisjointSet{
+    vector<int> rank,parent,size;
+    public:
+    DisjointSet(int n)
     {
-        visited[node]=1;
-        for(int i=0;i<isConnected[node].size();i++)
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for (int i = 0; i < parent.size(); i++)
         {
-            if(isConnected[node][i] == 1 && !visited[i])
-            {
-                dfs(i,visited,isConnected);
-            }
+            parent[i]=i;
+            size[i]=1;
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int v=isConnected.size();
-
-        vector<int> visited(v,0);
-        int numIslands=0;
-        for(int i=0;i<v;i++)
+    int findUltimateParent(int node)
+    {
+        if(node == parent[node])
         {
-            if(!visited[i])
+            return node;
+        }
+        else
+        {
+            return parent[node] = findUltimateParent(parent[node]);
+        }
+    }
+    
+    void unionByRank(int u,int v)
+    {
+        int ulp_u=findUltimateParent(u);
+        int ulp_v=findUltimateParent(v);
+
+        if(ulp_u == ulp_v)
+        {
+            return;
+        }
+        else if(rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v]=ulp_u;
+        }
+        else if(rank[ulp_u] == rank[ulp_v])
+        {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u,int v)
+    {
+        int ulp_u=findUltimateParent(u);
+        int ulp_v=findUltimateParent(v);
+
+        if(ulp_u == ulp_v)
+        {
+            return;
+        }
+
+        if(size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }   
+
+    }
+
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        DisjointSet ds(n);
+        for(int i=0;i<isConnected.size();i++)
+        {
+            for(int j=0;j<isConnected[i].size();j++)
             {
-                // cout<<i<<endl;
-                numIslands++;
-                dfs(i,visited,isConnected);
+                if(isConnected[i][j])
+                {
+                    ds.unionBySize(i,j);
+                }
+                
             }
         }
-        return numIslands;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.findUltimateParent(i) == i)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 };
