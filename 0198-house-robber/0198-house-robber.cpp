6ee51b1646a9b4d1eab1f0924cf9a4484367e1af
iaaryansood <bobmarley86325@gmail.com>
2024-12-22 20:47:55 +0530

    Time: 0 ms (100%), Space: 10.6 MB (26.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        if(nums.size() > 2)
+    int helper(vector<int> &nums,vector<int> &dp,int n)
+    {
+        if(n == 0 || n == 1)
         {
-            vector<int> dp(nums.size(),0);
-
-            dp[0]=nums[0];
-            dp[1]=max(nums[0],nums[1]);
-
-            for(int i=2;i<nums.size();i++)
-            {
-                dp[i]=max(dp[i-2] + nums[i] , dp[i-1]);
-            }
-            int n=nums.size();
-            return dp[n-1];
-        }
-        else
-        {
-            if(nums.size() == 1)
+            if(n == 0)
             return nums[0];
-
             else
             return max(nums[0],nums[1]);
         }
-        
+        else if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        else
+        {
+            int ans;
+            ans=max(helper(nums,dp,n-1),helper(nums,dp,n-2) + nums[n]);
+            dp[n]=ans;
+            return ans;
+        }
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n == 1 || n == 2)
+        {
+            if(n == 1)
+            return nums[0];
+            else
+            return max(nums[0],nums[1]);
+        }
+        vector<int> dp(n,-1);
+        helper(nums,dp,n-1);
+        return dp[n-1];
     }
 };
