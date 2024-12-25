60eca119a6a61eeb639a51ab43a77a8594253f7c
iaaryansood <bobmarley86325@gmail.com>
2024-12-25 12:11:43 +0530

    Time: 0 ms (100%), Space: 11.9 MB (15.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
-    int helper(vector<vector<int>> &obstacleGrid,vector<vector<int>> &dp,int n,int m)
-    {
-        if(n == 0 && m == 0)
-        {
-            if(obstacleGrid[0][0] == 0)
-            {
-                return dp[n][m] = 1;
-            }
-            if(obstacleGrid[0][0] == 1)
-            {
-                return dp[n][m] = 0;
-            }
-        }
-        if(n < 0 || m < 0)
-        {
-            return 0;
-        }
-        if(dp[n][m] != -1)
-        {
-            return dp[n][m];
-        }
-        else
-        {
-            int up=0,left=0;
-            if(n-1 >= 0 && obstacleGrid[n-1][m] == 0)
-            {
-                up = helper(obstacleGrid,dp,n-1,m);
-            }
-            if(m - 1 >= 0 && obstacleGrid[n][m-1] == 0)
-            {
-                left = helper(obstacleGrid,dp,n,m-1);
-            }
-            return dp[n][m] = up+left;
-        }
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
+        // cout<<n<<" "<<m;
         if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1)
         {
+            // cout<<"obstacle"<<endl;
             return 0;
         }
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(obstacleGrid,dp,n-1,m-1);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        dp[1][1] = 1;
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(i == 1 && j == 1)
+                {
+                    dp[1][1] = 1;
+                }
+                else if(obstacleGrid[i-1][j-1] == 1)
+                {
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        // cout<<endl;
+        // for(int i=0;i<n+1;i++)
+        // {
+        //     for(int j=0;j<m+1;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n][m];
     }
 };
